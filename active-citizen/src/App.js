54bd126f2b477cbfd3d8ccba7f642e9373c8d0bb
polya <polyakrupina@gmail.com>
2024-11-06 20:35:11 +0500

    create header

--- active-citizen/src/App.js
+++ active-citizen/src/App.js
@@ -1,9 +1,9 @@
-
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
--- active-citizen/src/index.js
+++ active-citizen/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/common.css';
+import './styles/reset.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
